0b626165b612d93d464837fc9b7ad5faa999a368
Nilesh2302 <118660370+Nilesh2302@users.noreply.github.com>
2023-02-22 09:30:32 +0530

    Added solution - LeetHub

--- Next Permutation - GFG/next-permutation.py
+++ Next Permutation - GFG/next-permutation.py
@@ -3,25 +3,46 @@
 class Solution:
     def nextPermutation(self, N, arr):
         # code here
+        # if N<=2:
+        #     return arr.reverse()
+        
+        # ptr = N-2
+        # while ptr>=0 and arr[ptr]>=arr[ptr+1]:
+        #     ptr -= 1
+        
+        # if ptr == -1:   #means it is the last combination and its reverse is first
+        #     arr.reverse()
+        #     return arr
+            
+        # for i in range(N-1,ptr,-1):
+        #     if arr[ptr]<arr[i]:
+        #         arr[ptr],arr[i] = arr[i],arr[ptr]
+        #         break
+        
+        # arr[ptr+1:] = reversed(arr[ptr+1:])        
+        # return arr 
+    
         if N<=2:
             return arr.reverse()
         
         ptr = N-2
         while ptr>=0 and arr[ptr]>=arr[ptr+1]:
-            ptr -= 1
+            ptr-=1
         
-        if ptr == -1:   #means it is the last combination and its reverse is first
+        #means given combination is the last combination is the last combination and
+        # next is reversed of it
+        if ptr==-1:
             arr.reverse()
             return arr
-            
+        
         for i in range(N-1,ptr,-1):
             if arr[ptr]<arr[i]:
                 arr[ptr],arr[i] = arr[i],arr[ptr]
                 break
         
-        arr[ptr+1:] = reversed(arr[ptr+1:])        
+        arr[ptr+1:] = reversed(arr[ptr+1:])  
         return arr    
-               
+                   
                 
 
 
