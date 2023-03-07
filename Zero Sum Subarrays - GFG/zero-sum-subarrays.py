0cbd852d3efac19add2a74d27a5ce4959d1f885c
Nilesh2302 <118660370+Nilesh2302@users.noreply.github.com>
2023-03-07 22:16:21 +0530

    Added solution - LeetHub

---
+++ Zero Sum Subarrays - GFG/zero-sum-subarrays.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    #Function to count subarrays with sum equal to 0.
+    def findSubArrays(self,arr,n):
+        
+        #return: count of sub-arrays having their sum equal to 0
+        a=0
+
+        j=0
+
+        d={0:1}
+
+        for i in range(n):
+
+            a+=arr[i]
+
+            if(a not in d):
+
+                d[a]=1
+
+            else:
+
+                j+=d[a]
+
+                d[a]+=1
+
+        return j
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+        
+if __name__=='__main__':
+    t=int(input())
+    for tc in range(t):
+        
+        N=int(input())
+        A = [int(x) for x in input().strip().split(' ')]
+        ob = Solution()
+        print(ob.findSubArrays(A,N))
+        
+                
+                
+# } Driver Code Ends
